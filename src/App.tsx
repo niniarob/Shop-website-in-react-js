e154ce3611d7f5cb6fc2054047c1ca6380fb1bf0
niniarob <robakidzenino89@gmail.com>
2023-11-21 13:17:45 +0400

    add router, single page functions and styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,21 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Header } from './categoriesPage/Header';
 import GlobalStyles from './GlobalStyles';
+import Product from './Product'; 
 import ProductDetails from './singleProductPage/ProductDetails'; 
-import Product from './Product';
 
 const App: React.FC = () => {
    return (
+   <Router>
     <div className="App">
       <GlobalStyles />
       <Header />
-      <Router>
         <Routes>
-          <Route path="/" element={<Product />} />
-          <Route path="/product-details/:title" element={<ProductDetails />} />
+        <Route path="/" element={<Product/>} />
+        <Route path="product-details/:title" element={<ProductDetails/>} />
         </Routes>
-      </Router>
     </div>
+    </Router>
   );
 };
 
